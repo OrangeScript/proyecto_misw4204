acaa9fd2d2f9bb24f6332fd1757e56f9c8ce45ef
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2024-04-14 14:48:56 -0500

    Poner direcciones de contenedores

--- app/worker.py
+++ app/worker.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     sleep(15)
     rabbitmq = RabbitMQ(HOST, QUEUE_NAME)
     rabbitmq.ensure_queue_exists()
-    db_url = "postgresql+psycopg2://postgres:postgres@35.202.0.137/drl_cloud"
+    db_url = "postgresql+psycopg2://postgres:postgres@db/drl_cloud"
 
     engine = create_engine(db_url)
 
